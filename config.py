8f41d3f6234e003b39720d6cc9dc0a26f6a726de
temirbayev1997 <abylaihant2@gmail.com>
2024-07-29 11:27:40 -0700

    result2

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_TOKEN = '6395218126:AAGk5RJZTYK60Mprpm3hms0PoHPAQMQjWaw'
 BITRIX_WEBHOOK_URL = 'https://b24-oph2lu.bitrix24.kz/rest/1/9wqbo0cc0mej450h/'
-ALLOWED_USERS = ['@kznzo', '@baimukhanbetova', '@Bekz_han', "@geroigazy"]
+ALLOWED_USERS = ['@kznzo', '@baimukhanbetova', '@Bekz_han', "@geroigazy", "@Diasbek123sus"]
