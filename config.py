a9ef4c6f39ec8c1367f8df6ae8e4ebd8622516f7
temirbayev1997 <abylaihant2@gmail.com>
2024-08-28 11:59:49 -0700

    config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_TOKEN = '6395218126:AAGk5RJZTYK60Mprpm3hms0PoHPAQMQjWaw'
 BITRIX_WEBHOOK_URL = 'https://b24-pilf17.bitrix24.kz/rest/1/73pqcp5f3ckz7bu1/'
-ALLOWED_USERS = ['@kznzo', '@baimukhanbetova', '', "", "", ""]
-CREATE_USERS = ['@kxnzo']
+ALLOWED_USERS = ['@kznzo', '@', '@bekz_han', "", "", ""]
+CREATE_USERS = ['@kxnzo', "@baimukhanbetova"]
