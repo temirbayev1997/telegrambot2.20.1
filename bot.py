bbacbf144aab3c2515a4f47f9b6904fb95a1f793
temirbayev1997 <abylaihant2@gmail.com>
2024-08-29 14:50:43 -0700

    gotovo

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ from config import API_TOKEN
 from handlers.common import register_handlers_common
 from handlers.photo import register_photo_handlers
 from handlers.congratulation import register_congratulation_handlers
-from handlers.help import register_help_command
 from handlers.booking import booking_user_handlers
 from handlers.user import register_user_handlers
 
@@ -24,14 +23,12 @@ dp.middleware.setup(LoggingMiddleware())
 register_handlers_common(dp)
 register_photo_handlers(dp)
 register_congratulation_handlers(dp)
-register_help_command(dp)
 booking_user_handlers(dp)
 register_user_handlers(dp)
 
 async def on_startup(dp):
     await bot.set_my_commands([
         BotCommand(command="/start", description="Начать работу с ботом"),
-        BotCommand(command="/help", description="Справка")
     ])
 
 if __name__ == "__main__":
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,7 +2,6 @@ from .common import register_handlers_common
 from .photo import register_photo_handlers
 from .user import register_user_handlers
 from .congratulation import register_congratulation_handlers
-from .help import register_help_command
 from .booking import booking_user_handlers
 
 
@@ -11,5 +10,4 @@ def register_handlers(dp):
     register_photo_handlers(dp)
     register_user_handlers(dp)
     register_congratulation_handlers(dp)
-    register_help_command(dp)
     booking_user_handlers(dp)
